f33b9f93710fcd443fa99452566f7698c132db6a
amine91480 <amine91480@hotmail.fr>
2021-01-25 19:37:54 +0100

    Add Parceable on Meeting for the next

--- app/src/main/java/com/amine/mareu/Model/Meeting.java
+++ app/src/main/java/com/amine/mareu/Model/Meeting.java
@@ -1,8 +1,11 @@
 package com.amine.mareu.Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.time.LocalTime;
 
-public class Meeting {
+public class Meeting implements Parcelable {
 
     private Integer id;
     private String date;
@@ -17,6 +20,30 @@ public class Meeting {
         this.subject = subject;
         this.participants = partcipants;
     }
+
+    protected Meeting(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readInt();
+        }
+        date = in.readString();
+        location = in.readString();
+        subject = in.readString();
+        participants = in.readString();
+    }
+
+    public static final Creator<Meeting> CREATOR = new Creator<Meeting>() {
+        @Override
+        public Meeting createFromParcel(Parcel in) {
+            return new Meeting(in);
+        }
+
+        @Override
+        public Meeting[] newArray(int size) {
+            return new Meeting[size];
+        }
+    };
 
     public Integer getId() {
         return id;
@@ -56,5 +83,24 @@ public class Meeting {
 
     public void setDate() {
         this.date = date;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (id == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeInt(id);
+        }
+        dest.writeString(date);
+        dest.writeString(location);
+        dest.writeString(subject);
+        dest.writeString(participants);
     }
 }
