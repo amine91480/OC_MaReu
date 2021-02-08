777a23135250e96314ceb7b531ee55eec9614cf1
amine91480 <amine91480@hotmail.fr>
2021-02-08 14:19:31 +0100

    Remove useless data and import, add method for action, select spinner and date/time and add the new Meeting

--- app/src/main/java/com/amine/mareu/Controller/AddNewMeeting.java
+++ app/src/main/java/com/amine/mareu/Controller/AddNewMeeting.java
@@ -8,6 +8,9 @@ import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.format.DateFormat;
@@ -28,6 +31,8 @@ import com.amine.mareu.Model.Meeting;
 import com.amine.mareu.R;
 import com.amine.mareu.Service.MeetingApiService;
 import com.amine.mareu.databinding.ActivityAddNewReunionBinding;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.sql.Time;
 import java.util.ArrayList;
@@ -40,7 +45,12 @@ public class AddNewMeeting extends AppCompatActivity implements AdapterView.OnIt
 
     private ActivityAddNewReunionBinding binding;
     private MeetingApiService mApiService;
+    private TextView mDate;
+
+    // Element View
     private String spinner;
+    private DatePickerDialog mDateSetListener;
+    private TimePickerDialog mTimePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,14 +65,12 @@ public class AddNewMeeting extends AppCompatActivity implements AdapterView.OnIt
     }
 
     private void receipData() {
+        // Spinner for choose the Room of the Meeting -> OK
         spinnerOption();
-
-        binding.addMeeting.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                addMeeting();
-                }
-            }
-        );
+        // Open Dialogue Alert to choose a Date -> OK but the Time not Working :/
+        dialogDate();
+        // Give data for Add a new Meeting -> OK
+        addMeeting();
     }
 
     public void spinnerOption() {
@@ -90,17 +98,58 @@ public class AddNewMeeting extends AppCompatActivity implements AdapterView.OnIt
     }
 
     private void addMeeting() {
-        Integer size;
-        size = mApiService.getMeetings().size();
-        String participant, subject;
+        binding.addMeeting.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            Integer size;
+            size = mApiService.getMeetings().size();
+            String participant, subject, date;
 
+            participant = binding.participant.getText().toString();
+            subject = binding.subject.getText().toString();
+            date = binding.time.getText().toString();
 
-        participant = binding.participant.getText().toString();
-        subject = binding.subject.getText().toString();
-        Date date = new Date();
-        Meeting meeting = new Meeting(size, date.toString(), spinner, subject, participant );
-        mApiService.createMeeting(meeting);
-        finish();
+            Meeting meeting = new Meeting(size, date, spinner, subject, participant);
+            mApiService.createMeeting(meeting);
+            finish();
+            }
+            }
+        );
     }
 
+    private void dialogDate() {
+        Calendar date = Calendar.getInstance();
+        int mDay = date.get(Calendar.DAY_OF_MONTH);
+        int mMonth = date.get(Calendar.MONTH);
+        int mYear = date.get(Calendar.YEAR);
+        int mHour = date.get(Calendar.HOUR);
+        int mMinute = date.get(Calendar.MINUTE);
+
+        binding.info.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("LongLogTag")
+            public void onClick(View v) {
+                mDateSetListener = new DatePickerDialog(AddNewMeeting.this, new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        date.set(year, month, dayOfMonth);
+                        binding.date.setText(DateFormat.getDateFormat(getApplicationContext()).format(date.getTime()));
+
+                        mTimePickerDialog = new TimePickerDialog(AddNewMeeting.this, new TimePickerDialog.OnTimeSetListener() {
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                date.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                date.set(Calendar.MINUTE, minute);
+                                binding.time.setText(DateFormat.getTimeFormat(getApplicationContext()).format(date.getTime()));
+                                Log.d("HEURE //////////////////////////////////////////////////:", String.valueOf(date.getTime()));
+                                Log.d("DATE //////////////////--------------------------------------------------------------", String.valueOf(date.getTime()));
+                            }
+                        }, mHour, mMinute, false);
+
+                        mTimePickerDialog.show();
+                    }
+                }, mYear, mMonth, mDay);
+
+                Log.d("----------------------------------------------------------------------------------", String.valueOf(date));
+                mDateSetListener.show();
+            }
+        });
+
+    }
 }
