9dcbe40111aedaac623eb0e8a1df253b502f7abd
amine91480 <amine91480@hotmail.fr>
2021-02-24 19:00:13 +0100

    Rollback to delate Parceable for connexion on Metting and Room

--- app/src/main/java/com/amine/mareu/Model/Meeting.java
+++ app/src/main/java/com/amine/mareu/Model/Meeting.java
@@ -5,16 +5,16 @@ import android.os.Parcelable;
 
 import java.util.Date;
 
-public class Meeting implements Parcelable {
+public class Meeting {
 
     private Integer id;
     private Date dateBegin;
     private Date dateFinish;
-    private String room;
+    private Room room;
     private String subject;
     private String participants;
 
-    public Meeting(Integer id, Date dateBegin, Date dateFinish, String room, String subject, String partcipants) {
+    public Meeting(Integer id, Date dateBegin, Date dateFinish, Room room, String subject, String partcipants) {
         this.id = id;
         this.dateBegin = dateBegin;
         this.dateFinish = dateFinish;
@@ -31,30 +31,6 @@ public class Meeting implements Parcelable {
         }
     }
 
-    protected Meeting(Parcel in) {
-        if (in.readByte() == 0) {
-            id = null;
-        } else {
-            id = in.readInt();
-        }
-        dateBegin = (Date) in.readSerializable();
-        dateFinish = (Date) in.readSerializable();
-        room = in.readString();
-        subject = in.readString();
-        participants = in.readString();
-    }
-
-    public static final Creator<Meeting> CREATOR = new Creator<Meeting>() {
-        @Override
-        public Meeting createFromParcel(Parcel in) {
-            return new Meeting(in);
-        }
-
-        @Override
-        public Meeting[] newArray(int size) {
-            return new Meeting[size];
-        }
-    };
 
     public Integer getId() {
         return id;
@@ -64,11 +40,11 @@ public class Meeting implements Parcelable {
         this.id = id;
     }
 
-    public String getRoom() {
+    public Room getRoom() {
         return room;
     }
 
-    public void setRoom(String room) {
+    public void setRoom(Room room) {
         this.room = room;
     }
 
@@ -104,23 +80,4 @@ public class Meeting implements Parcelable {
         this.dateFinish = dateFinish;
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        if (id == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeInt(id);
-        }
-        dest.writeSerializable(dateBegin);
-        dest.writeSerializable(dateFinish);
-        dest.writeString(room);
-        dest.writeString(subject);
-        dest.writeString(participants);
-    }
 }
