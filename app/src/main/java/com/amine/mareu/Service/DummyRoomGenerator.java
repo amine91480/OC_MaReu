dd7a769d029339ea0160ec5cfa6726130dc2a294
amine91480 <amine91480@hotmail.fr>
2021-02-27 17:22:33 +0100

    Change the Attribute color Ressource color to the hexadecimal color -> OK

--- app/src/main/java/com/amine/mareu/Service/DummyRoomGenerator.java
+++ app/src/main/java/com/amine/mareu/Service/DummyRoomGenerator.java
@@ -10,11 +10,10 @@ import java.util.List;
 public abstract class DummyRoomGenerator {
 
     public static List<Room> DUMMY_ROOM = Arrays.asList(
-            new Room("Room A", R.color.purple_500),
-            new Room("Room B",R.color.blueblue),
-            new Room("Room C",R.color.teal_200),
-            new Room("Room D",R.color.teal_700),
-            new Room("Room A",R.color.purple_200)
+            new Room("Room A","#2F4F4F"),
+            new Room("Room B","#FFF8DC"),
+            new Room("Room C","#A52A2A"),
+            new Room("Room D","#7CFC00")
     );
 
     static List<Room> generateRoom() {
