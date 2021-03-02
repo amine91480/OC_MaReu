c5d033cf87fc35d669211b6aa9d9f7236d7ef6c4
amine91480 <amine91480@hotmail.fr>
2021-03-02 15:22:49 +0100

    Indentation -> OK Log-D comment -> OK Add TimeUnite for convert  long -> OK Create a method RandomNumber to return the hours between 7 to 19 -> OK Create a method getRandomDate to return a List Date -> OK Create method getDummyMeeting to return a new Meeting List(id, getRandomDate(RandomNumber), the same with hours +1, Room(randomNumber.size),subjet,mail) -> OK

--- app/src/main/java/com/amine/mareu/Service/DummyMeetingGenerator.java
+++ app/src/main/java/com/amine/mareu/Service/DummyMeetingGenerator.java
@@ -1,38 +1,39 @@
 package com.amine.mareu.Service;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
 import com.amine.mareu.Model.Meeting;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import static com.amine.mareu.Service.DummyRoomGenerator.*;
 
-public abstract class DummyMeetingGenerator {
+public class DummyMeetingGenerator {
 
-    // ICi on va géré la 2.0 Meeting avec des vraies Date Mamène !
-   /* public static List<Meeting> getDummyMeeting(){
-        List<Meeting> test = new ArrayList<>();
+    public static int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
 
+    public static ArrayList<Date> getRandomDate() {
+        ArrayList<Date> randDate = new ArrayList<Date>();
+        randDate.add(new Date(2021, 00, 01, getRandomNumber(07, 19), 00, 00));
+        randDate.add(new Date(2021, 00, 01, getRandomNumber(07, 19), 00, 00));
+        randDate.add(new Date(2021, 00, 01, getRandomNumber(07, 19), 00, 00));
+        randDate.add(new Date(2021, 00, 01, getRandomNumber(07, 19), 00, 00));
+        return randDate;
+    }
 
+    public static List<Meeting> getDummyMeeting() {
+        Integer i = 0;
+        List<Meeting> test = new ArrayList<Meeting>();
 
+        for (i = 0; i < getRandomDate().size(); i++) {
+            Date theDate = getRandomDate().get(i);
+            long theDateFinish = theDate.getTime() + TimeUnit.HOURS.toMillis(1);
+            test.add(new Meeting(1, theDate, new Date(theDateFinish), generateRoom().get(getRandomNumber(0, generateRoom().size())), "Android", "flegard91480@hotmail.fr"));
+            System.out.println(test.get(i).getDateBegin() + "YESSSSSSSSS" + test.get(i).getDateAfter());
+        }
         return test;
-    }*/
-
-    public static List<Meeting> DUMMY_MEETING = Arrays.asList(
-            new Meeting(1, new Date(), new Date(), DummyRoomGenerator.generateRoom().get(0), "Android", "amine91480@hotmail.fr"),
-            new Meeting(2, new Date(), new Date(), DummyRoomGenerator.generateRoom().get(1), "JAVA", "flegard91480@hotmail.fr"),
-            new Meeting(3, new Date(), new Date(), DummyRoomGenerator.generateRoom().get(2), "Kotlin", "valentin91, tuto45@hotmail.fr"),
-            new Meeting(4, new Date(), new Date(), DummyRoomGenerator.generateRoom().get(3), "Front", "Caroline@hotmail.fr")
-    );
-
-    static List<Meeting> generateMeetings() {
-        return new ArrayList<>(DUMMY_MEETING);
     }
 }
