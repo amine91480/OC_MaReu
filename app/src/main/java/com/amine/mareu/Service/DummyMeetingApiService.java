21bf7396045e676bb3b2a1dab5e1d80fb67d532b
amine91480 <amine91480@hotmail.fr>
2021-03-02 15:17:49 +0100

    Indentation -> Delete Parse -> OK Log-D comment -> OK Service take the new DummyMeetingApiService -> OK list meetings take DummyMeetingGenerator.getDummyMeeting method -> OK

--- app/src/main/java/com/amine/mareu/Service/DummyMeetingApiService.java
+++ app/src/main/java/com/amine/mareu/Service/DummyMeetingApiService.java
@@ -1,14 +1,17 @@
 package com.amine.mareu.Service;
 
-import android.util.Log;
-
 import com.amine.mareu.Model.Meeting;
 
 import java.util.List;
 
+import android.util.Log;
+
 public class DummyMeetingApiService implements MeetingApiService {
 
-    private List<Meeting> meetings = DummyMeetingGenerator.generateMeetings();
+    private List<Meeting> meetings = DummyMeetingGenerator.getDummyMeeting();
+
+    public DummyMeetingApiService() {
+    }
 
     @Override
     public List<Meeting> getMeetings() {
@@ -30,19 +33,19 @@ public class DummyMeetingApiService implements MeetingApiService {
 
                 // Vérifier si date de début de meeting n'est pas entre le date de début du meet et la date de fin de meet
                 if ((meeting.getDateBegin().after(meet.getDateBegin())) && (meeting.getDateBegin().before(meet.getDateAfter()))) {
-                    Log.d("isReserve/API", "Date de debut et entre l'intervalle");
+                    //Log.d("isReserve/API", "Date de debut et entre l'intervalle");
                     isReserved = true;
                     return true;
                 }
                 // Vérifier si la date de fin de meeting n'est pas entre la date de début du meet et la date de fin du meet
                 if ((meeting.getDateAfter().after(meet.getDateBegin())) && (meeting.getDateAfter().before(meet.getDateAfter()))) {
-                    Log.d("isReserve/API", "Date de fin et entre l'intervalle");
+                    // Log.d("isReserve/API", "Date de fin et entre l'intervalle");
                     isReserved = true;
                     return isReserved;
                 }
                 // Vérifier si la date du début de meeting n'est pas égal au meet
                 if (meeting.getDateBegin().equals(meet.getDateBegin())) {
-                    Log.d("isReserve/API", "date identiaque");
+                    // Log.d("isReserve/API", "date identiaque");
                     isReserved = true;
                     return true;
                 }
@@ -51,10 +54,8 @@ public class DummyMeetingApiService implements MeetingApiService {
         return isReserved;
     }
 
-
     @Override
     public void deleteMeeting(Meeting meeting) {
         meetings.remove(meeting);
     }
-
 }
