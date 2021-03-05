98c09048f6ca64774f9ca9419927d93cd0b2c6b8
amine91480 <amine91480@hotmail.fr>
2021-03-05 15:25:36 +0100

    Delete List Participant and import Calendar

--- app/src/main/java/com/amine/mareu/Service/MeetingApiService.java
+++ app/src/main/java/com/amine/mareu/Service/MeetingApiService.java
@@ -1,20 +1,20 @@
 package com.amine.mareu.Service;
 
 import com.amine.mareu.Model.Meeting;
+import com.amine.mareu.Model.Room;
 
+import java.util.Date;
 import java.util.List;
 
 public interface MeetingApiService {
 
     List<Meeting> getMeetings();
 
-    /*List<String> getParticpants();*/
-
     void createMeeting(Meeting meeting);
 
     void deleteMeeting(Meeting meeting);
 
-    boolean isReserved(Meeting meeting);
+    boolean isReserved(Date debut, Date fin, Room room);
 
     /*List<String> checkIfEmailIsOK(String str);*/
 
