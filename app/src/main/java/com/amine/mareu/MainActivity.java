85bfb271e2286b2880b2ba12f05335ae2401f997
amine91480 <amine91480@hotmail.fr>
2021-03-11 21:50:51 +0100

    Implemente onOptionItemSelected -> OK On menu icone filter set LinearLayout visible -> OK the icone search set the Linear gone -> OK Implemente method  filterRoom and the roomChoose -> OK

--- app/src/main/java/com/amine/mareu/MainActivity.java
+++ app/src/main/java/com/amine/mareu/MainActivity.java
@@ -10,6 +10,7 @@ import com.amine.mareu.Controller.AddNewMeeting;
 import com.amine.mareu.Controller.MyListMeetingAdapter;
 import com.amine.mareu.DI.DI;
 import com.amine.mareu.Model.Meeting;
+import com.amine.mareu.Model.Room;
 import com.amine.mareu.Service.MeetingApiService;
 import com.amine.mareu.databinding.ActivityMainBinding;
 
@@ -23,7 +24,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -36,9 +39,11 @@ public class MainActivity extends AppCompatActivity {
     private MeetingApiService mApiService;
     private List<Meeting> mMeetingList;
     private List<Meeting> mMeetingListCopy;
+    private List<Room> mRoomList;
 
-    private RecyclerView mRecyclerView;
     private MyListMeetingAdapter mAdapter;
+
+    private String selectedRoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +75,9 @@ public class MainActivity extends AppCompatActivity {
     private void setupData() {
         mApiService = DI.getMeetingApiService();
         mMeetingList = mApiService.getMeetings();
+        mRoomList = mApiService.getRooms();
+        mMeetingListCopy = new ArrayList<>();
+        mMeetingListCopy.addAll(mMeetingList);
     }
 
     @Override
@@ -86,11 +94,67 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                mAdapter = new MyListMeetingAdapter(mMeetingList, getApplicationContext());
                 mAdapter.getFilter().filter(newText);
                 return false;
             }
         });
         return true;
     }
-}
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.button_menu:
+                Toast.makeText(getApplicationContext(), "Watch this super New Item", Toast.LENGTH_SHORT);
+                binding.newFilter.setVisibility(View.VISIBLE);
+                return true;
+            case R.id.action_search:
+                binding.newFilter.setVisibility(View.GONE);
+                selectedRoom = null;
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void filtredRoom(String selected) {
+        Log.d("Room", "Filtre Test !: ");
+        selectedRoom = selected;
+
+        ArrayList<Meeting> filtredRoom = new ArrayList<Meeting>();
+
+        if (selectedRoom.isEmpty()) {
+            Log.d("Room", "Render the List Begin ");
+            mAdapter = new MyListMeetingAdapter(mMeetingList, mContext);
+            binding.myRecyclerView.setAdapter(mAdapter);
+        }
+        for (Meeting meeting : mMeetingList) {
+            if (meeting.getRoom().toString().toLowerCase().contains(selectedRoom.toLowerCase())) {
+                filtredRoom.add(meeting);
+            }
+            mAdapter = new MyListMeetingAdapter(filtredRoom, mContext);
+            binding.myRecyclerView.setAdapter(mAdapter);
+        }
+    }
+
+    public void RoomAFilterChoose(View view) {
+        Log.d("Room", "Touched: " + mRoomList.get(0).toString());
+        filtredRoom(mRoomList.get(0).toString());
+    }
+
+    public void RoomBFilterChoose(View view) {
+        Log.d("Room", "Touched: " + mRoomList.get(1));
+        filtredRoom(mRoomList.get(1).toString());
+    }
+
+    public void RoomCFilterChoose(View view) {
+        Log.d("Room", "Touched: " + mRoomList.get(2));
+        filtredRoom(mRoomList.get(2).toString());
+    }
+
+    public void RoomDFilterChoose(View view) {
+        Log.d("Room", "Touched: ");
+        filtredRoom(mRoomList.get(3).toString());
+    }
+}
