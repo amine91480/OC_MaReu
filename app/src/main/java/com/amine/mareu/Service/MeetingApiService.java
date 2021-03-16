7d85d23d0d092a7fe2f72d5b9f687679714a644c
amine91480 <amine91480@hotmail.fr>
2021-03-16 21:17:17 +0100

    Indentation -> OK Check and Comment method -> OK

--- app/src/main/java/com/amine/mareu/Service/MeetingApiService.java
+++ app/src/main/java/com/amine/mareu/Service/MeetingApiService.java
@@ -10,17 +10,19 @@ import java.util.List;
 
 public interface MeetingApiService {
 
-    List<Meeting> getMeetings();
+    List<Meeting> getMeetings(); // Render the List of Meeting
 
-    List<Room> getRooms();
+    List<Room> getRooms(); // Render the List of Room
 
-    ArrayList<Meeting> chooseYourRoom(Room room);
+    List<String> getListNameRooms(); // Render the List of Room Name(Use to AutoCompleteView)
 
-    void createMeeting(Meeting meeting);
+    ArrayList<Meeting> roomChoseToFilter(Room room); // Take a Room to return all Metting in this Room
 
-    void deleteMeeting(Meeting meeting);
+    void createMeeting(Meeting meeting); // Create a New Meeting to add on the List Meeting
 
-    boolean isReserved(LocalDateTime debut, LocalDateTime fin, Room room);
-    /*List<String> checkIfEmailIsOK(String str);*/
+    void deleteMeeting(Meeting meeting); // Deleate the Meeting to the List
 
+    boolean checkTheFuturReservation(LocalDateTime debut, LocalDateTime fin, Room room); // Take Time and Room to check and return True or False
+
+    /*List<String> checkIfEmailIsOK(String str); // Check if the String is a Email valide to Add on a List of Meeting */
 }
