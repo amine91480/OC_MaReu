d6eeb9fe0f9411db04ab763dbffd5115c294c95a
amine91480 <amine91480@hotmail.fr>
2021-03-21 20:12:27 +0100

    Indent Warning -> Ok -> Import not use -> Delete

--- app/src/main/java/com/amine/mareu/Service/MeetingApiService.java
+++ app/src/main/java/com/amine/mareu/Service/MeetingApiService.java
@@ -3,26 +3,32 @@ package com.amine.mareu.Service;
 import com.amine.mareu.Model.Meeting;
 import com.amine.mareu.Model.Room;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public interface MeetingApiService {
 
     List<Meeting> getMeetings(); // Render the List of Meeting
 
+    // Render the List of Meeting filtered by LocalDate and Room
+    List<Meeting> getFilteredMeeting(Room room, LocalDate date);
+
     List<Room> getRooms(); // Render the List of Room
 
     List<String> getListNameRooms(); // Render the List of Room Name(Use to AutoCompleteView)
-
-    ArrayList<Meeting> roomChoseToFilter(Room room); // Take a Room to return all Metting in this Room
 
     void createMeeting(Meeting meeting); // Create a New Meeting to add on the List Meeting
 
     void deleteMeeting(Meeting meeting); // Deleate the Meeting to the List
 
-    boolean checkTheFuturReservation(LocalDateTime debut, LocalDateTime fin, Room room); // Take Time and Room to check and return True or False
+    List<Meeting> roomChooseToFilter(Room room); // Take a Room to return all Metting in this Room
+
+    // Take a LocalDate(Year,Month,Day) to return all Meeting in this Date
+    List<Meeting> dateChooseToFilter(LocalDate date);
+
+    // Take Time and Room to check and return True or False
+    boolean checkTheFuturReservation(LocalDateTime debut, LocalDateTime fin, Room room);
 
     /*List<String> checkIfEmailIsOK(String str); // Check if the String is a Email valide to Add on a List of Meeting */
 }
